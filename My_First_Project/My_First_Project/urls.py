c3578f13404b634922fa1370382b2dad11763f07
Nibir <mz.nibir@gmail.com>
2021-05-05 17:08:33 +0600

    view and url practise

---
+++ My_First_Project/My_First_Project/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from first_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='index'),
+    path('', views.home, name='index'),
+
+]
