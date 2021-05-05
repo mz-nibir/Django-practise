fa9e6d6032f5ff3c27a27c52faafbb6174771f24
Nibir <mz.nibir@gmail.com>
2021-05-05 17:46:50 +0600

    another exp of view and url practice

--- My_First_Project/My_First_Project/urls.py
+++ My_First_Project/My_First_Project/urls.py
@@ -5,8 +5,8 @@ from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
-    path('home/', views.home, name='index'),
-    path('', views.home, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 
 ]
--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello world!<h1>')
+    return HttpResponse('<h1>Hello world!</h1>')
 
 def home(request):
-    return HttpResponse('<h2>Hello Nibir!<h2>')
+    return HttpResponse("<h1>This is Homepage!</h1> <a href='/contact/'>Contact</a> <br> <a href='about/'>About</a>")
+
+def contact(request):
+    return HttpResponse("<h1>This is Contact Page </h1> <a href='/'>Home</a> <br> <a href='/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("<h1>About Us </h1> <a href='/'>Home</a> <br> <a href='/contact/'>Contact</a>")
