b4fe26d6fc621c6c616013538f8c1e8d1b027d26
Nibir <mz.nibir@gmail.com>
2021-05-08 15:14:00 +0600

    one function added to model.py to view direct data entry

--- My_First_Project/first_app/models.py
+++ My_First_Project/first_app/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Musician (models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    instrument =models.CharField(max_length=100)
+    instrument =models.CharField(max_length=100)\
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name 
 
 class Albam (models.Model):
     artist = models.ForeignKey(Musician, on_delete= models.CASCADE)
