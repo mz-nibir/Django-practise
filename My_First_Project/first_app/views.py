04ff298fa7dbeb9a19b1d5f2f91e209138a98c7e
Nibir <mz.nibir@gmail.com>
2021-05-09 00:15:36 +0600

    view templates model together

--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Musician,Albam
 
 # Create your views here.
 
 def index(request):
-    diction={'text_1': 'i am from views.py page'}
+    musician_list= Musician.objects.order_by('first_name')
+    diction={'text_1': 'The list of Musician','musician': musician_list}
     return render(request,'first_app/index.html',context= diction)
