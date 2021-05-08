fbf37f28d124f42e96a8fe74ec7089e062765c2e
Nibir <mz.nibir@gmail.com>
2021-05-08 22:46:55 +0600

    more example of model practise and rating added

--- My_First_Project/first_app/models.py
+++ My_First_Project/first_app/models.py
@@ -8,10 +8,22 @@ class Musician (models.Model):
     instrument =models.CharField(max_length=100)\
 
     def __str__(self):
-        return self.first_name + " " + self.last_name 
+        return self.first_name + " " + self.last_name
 
 class Albam (models.Model):
     artist = models.ForeignKey(Musician, on_delete= models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
+    rating= (
+    (1,'worst'),
+    (2,'bad'),
+    (3,'not bad'),
+    (4,'good'),
+    (5,'Excellent!'),
+    )
+    num_stars= models.IntegerField(choices=rating)
+
+    def __str__(self):
+        return self.name + ", rating: "+ str(self.num_stars) 
