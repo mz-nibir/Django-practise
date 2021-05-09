ec473bbe9831b90a97dad52616954b45e3dfdfa9
Nibir <mz.nibir@gmail.com>
2021-05-10 00:04:47 +0600

    django form practise

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 class user_form (forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label='full name',initial='nibir')
 
-    user_email= forms.EmailField()
-    
+    user_email= forms.EmailField(label='User Email')
