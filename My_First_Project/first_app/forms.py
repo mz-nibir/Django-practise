f324dc6e3624f9223d2b5d44a41c9936487bb788
Nibir <mz.nibir@gmail.com>
2021-05-10 00:28:03 +0600

    django widget practise

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class user_form (forms.Form):
-    user_name = forms.CharField(label='full name',initial='nibir')
+    user_name = forms.CharField(label='full name',widget=forms.TextInput(attrs={'placeholder':'please write your name','style':'width:300px'}))
 
     user_email= forms.EmailField(label='User Email')
+
+    dob = forms.DateField(label='Date of Birth', widget=forms.TextInput(attrs={'type':'date'}))
