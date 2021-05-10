879c16cc761d798521f4f5bf7da703066e982b85
Nibir <mz.nibir@gmail.com>
2021-05-11 01:18:45 +0600

    field exp

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class user_form (forms.Form):
-    field = forms.CharField(max_length=15,min_length=5)
+    # field = forms.CharField(max_length=15,min_length=5)
+    choice= (('','select'),('A','A'),('B','B'),('C','C'))
+    field = forms.MultipleChoiceField(choices=choice, widget=forms.CheckboxSelectMultiple)
