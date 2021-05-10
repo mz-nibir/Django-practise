813770afa87bdfa0c462d771c71d238a5b093961
Nibir <mz.nibir@gmail.com>
2021-05-10 16:35:25 +0600

    form submission

--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -13,4 +13,20 @@ def index(request):
 def form(request):
     new_form = forms.user_form()
     diction={'text_form': new_form, 'heading_1': 'this is from django form'}
+
+    if request.method == 'POST':
+        new_form = forms.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            dob = new_form.cleaned_data['dob']
+            user_email = new_form.cleaned_data['user_email']
+
+            diction.update({'user_name': user_name})
+            diction.update({'user_dob': dob})
+            diction.update({'user_email': user_email})
+
+            diction.update({'form_submitted': 'yes'})
+
+
     return render(request,'first_app/form.html',context= diction)
