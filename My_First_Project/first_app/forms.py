3ee7ed9fd6ab0439bb39cfdfb933d4a25204444d
Nibir <mz.nibir@gmail.com>
2021-05-12 01:55:08 +0600

    django model form

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -1,12 +1,24 @@
 from django import forms
 from django.core import validators
+from first_app import models
 
-def even_or_not(value):
-    if value%2 == 1:
-        raise forms.ValidationError('Please insert a Even Number')
+# class user_form (forms.Form):
+#
+#     # field = forms.CharField(validators=[validators.MaxLengthValidator(10),validators.MinLengthValidator(5)])
+#     # field = forms.IntegerField(validators=[validators.MaxValueValidator(10),validators.MinValueValidator(5)])
+#     # field = forms.IntegerField(validators=[even_or_not])
+#     user_email= forms.EmailField()
+#     user_vemail= forms.EmailField()
+#
+#     def clean(self):
+#         all_clean_data=super().clean()
+#         u_email=all_clean_data['user_email']
+#         v_email=all_clean_data['user_vemail']
+#
+#         if u_email != v_email :
+#             raise forms.ValidationError('Field is not match')
 
-class user_form (forms.Form):
-
-    # field = forms.CharField(validators=[validators.MaxLengthValidator(10),validators.MinLengthValidator(5)])
-    # field = forms.IntegerField(validators=[validators.MaxValueValidator(10),validators.MinValueValidator(5)])
-    field = forms.IntegerField(validators=[even_or_not])
+class Musician_form(forms.ModelForm):
+    class Meta:
+        model= models.Musician
+        fields= "__all__"
--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -11,22 +11,16 @@ def index(request):
     return render(request,'first_app/index.html',context= diction)
 
 def form(request):
-    new_form = forms.user_form()
-    diction={'text_form': new_form, 'heading_1': 'this is from django form'}
-
+    new_form = forms.Musician_form()
     if request.method == 'POST':
-        new_form = forms.user_form(request.POST)
-        diction.update({'text_form': new_form})
+        new_form= forms.Musician_form(request.POST)
 
         if new_form.is_valid():
-            user_name = new_form.cleaned_data['field']
+            new_form.save(commit=True)
+            return index(request)
 
+    diction={'test_form':new_form,'heading_1':'add new musician'}
 
-            diction.update({'user_name': user_name})
-            # diction.update({'user_dob': dob})
-            # diction.update({'user_email': user_email})
-
-            diction.update({'form_submitted': 'yes'})
 
 
     return render(request,'first_app/form.html',context=diction)
