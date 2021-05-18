d0ae818bc15c8b572092a248d658b7fdf8ec1a80
Nibir <mz.nibir@gmail.com>
2021-05-18 23:24:44 +0600

    relative urls

--- My_First_Project/first_app/urls.py
+++ My_First_Project/first_app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.urls import path
 from first_app import views
 
+app_name= "First_app"
+
 urlpatterns= [
     path('',views.index,name='index'),
     path('form/',views.form,name='form'),
