f956328122447aab9c3bdaa4c51974491174aa1b
Nibir <mz.nibir@gmail.com>
2021-05-22 13:06:02 +0600

    Custom Filter with argument

--- My_First_Project/first_app/templatetags/my_filter.py
+++ My_First_Project/first_app/templatetags/my_filter.py
@@ -2,8 +2,8 @@ from django import template
 
 register = template.Library()
 
-def my_fiter(value):
-    return value + " This is starting from Custom filter"
+def my_fiter(value,args):
+    return value + " " + args
 
 
 register.filter('custom_filter',my_fiter)
