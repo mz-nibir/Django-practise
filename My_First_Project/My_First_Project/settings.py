ae57d3f5966f66de3deaa9d2396fe26017b6384f
Nibir <mz.nibir@gmail.com>
2021-05-23 12:38:17 +0600

    mysql db connect to project

--- My_First_Project/My_First_Project/settings.py
+++ My_First_Project/My_First_Project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'My_First_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_first_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
