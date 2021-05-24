051015c84aa81801d3843e558df5addecff91307
Nibir <mz.nibir@gmail.com>
2021-05-24 17:04:11 +0600

    Entity Forms in CRUD

--- My_First_Project/first_app/forms.py
+++ My_First_Project/first_app/forms.py
@@ -2,23 +2,14 @@ from django import forms
 from django.core import validators
 from first_app import models
 
-# class user_form (forms.Form):
-#
-#     # field = forms.CharField(validators=[validators.MaxLengthValidator(10),validators.MinLengthValidator(5)])
-#     # field = forms.IntegerField(validators=[validators.MaxValueValidator(10),validators.MinValueValidator(5)])
-#     # field = forms.IntegerField(validators=[even_or_not])
-#     user_email= forms.EmailField()
-#     user_vemail= forms.EmailField()
-#
-#     def clean(self):
-#         all_clean_data=super().clean()
-#         u_email=all_clean_data['user_email']
-#         v_email=all_clean_data['user_vemail']
-#
-#         if u_email != v_email :
-#             raise forms.ValidationError('Field is not match')
 
-class Musician_form(forms.ModelForm):
+class MusicianForm(forms.ModelForm):
     class Meta:
         model= models.Musician
+        fields= "__all__"
+
+class AlbamForm(forms.ModelForm):
+    release_date= forms.DateField(widget= forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model= models.Albam
         fields= "__all__"
--- My_First_Project/first_app/views.py
+++ My_First_Project/first_app/views.py
@@ -6,7 +6,8 @@ from first_app import forms
 # Create your views here.
 
 def index(request):
-    diction={'title':'Home Page'}
+    musician_list= Musician.objects.order_by('first_name')
+    diction={'title':'Home Page','musician_list':musician_list}
     return render(request,'first_app/index.html',context= diction)
 
 def albam_list(request):
@@ -14,9 +15,27 @@ def albam_list(request):
     return render(request,'first_app/albam_list.html',context= diction)
 
 def musician_form(request):
-    diction={'title':'Add Musician'}
+    form = forms.MusicianForm()
+
+    if request.method == 'POST':
+        form = forms.MusicianForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+    diction={'title':'Add Musician','musician_form':form}
     return render(request,'first_app/musician_form.html',context= diction)
 
 def albam_form(request):
-    diction={'title':'Add Albam'}
+    form=forms.AlbamForm()
+
+    if request.method == 'POST':
+        form = forms.AlbamForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+    diction={'title':'Add Albam','albam_form':form}
     return render(request,'first_app/albam_form.html',context= diction)
